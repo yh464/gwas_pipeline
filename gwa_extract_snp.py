060b5b1b72fbad787d513c4883eaa5cd03306aca
Yuankai He <yh464@cam.ac.uk>
2025-03-10 22:47:06 +0000

    Improved extract SNP utility to output long format table

--- gwa_extract_snp.py
+++ gwa_extract_snp.py
@@ -26,15 +26,21 @@ def main(args):
             if fnmatch(y, '*_X.fastGWA'): continue
             if fnmatch(y, '*_all_chrs.fastGWA'): continue
             if not fnmatch(y, '*.fastGWA') or fnmatch(y, '*.txt') or fnmatch(y, '*.tsv'): continue
-            df = pd.read_table(y, sep = '\s+', usecols = ['SNP', 'P'], index_col = 'SNP')
+            df = pd.read_table(y, sep = '\s+', usecols = ['SNP','BETA','SE','P'], index_col = 'SNP')
+            df = df.loc[[args.snp],:].reset_index(names='SNP')
             prefix = '.'.join(y.split('.')[:-1]) # remove extension
-            df.columns = [prefix]
-            all_files.append(df.loc[args.snp,prefix])
+            df['Phenotype'] = prefix
+            df['Group'] = x
+            df['Z'] = df.BETA/df.SE
+            all_files.append(df[['Group','Phenotype','SNP','BETA','Z','P']])
         
-    all_files = pd.concat(all_files, axis = 1)
-    all_files.insert(loc = 0, column = 'min_pval', value = all_files.min(axis = 1))
-    print(all_files)
-    all_files.to_clipboard(index = True)
+    all_files = pd.concat(all_files)
+    if args.out != None: all_files.to_csv(args.out, sep = '\t', index = False)
+    all_files.to_clipboard(index = False)
+    all_files_wide = all_files.pivot_table(values = 'P', index = 'SNP', columns = 'Phenotype')
+    all_files_wide.insert(loc = 0, column = 'min_pval', value = all_files.min(axis = 1))
+    print(all_files_wide)
+    
     
 if __name__ == '__main__':
     import argparse
@@ -45,6 +51,7 @@ if __name__ == '__main__':
       nargs = '*', default = ['global','global_asym'])
     parser.add_argument('-i','--in', dest = '_in', help = 'Directory containing all GWA summary statistics',
       default = '../gwa/')
+    parser.add_argument('-o','--out', dest = 'out', help = 'Output list file')
     args = parser.parse_args()
     import os
     args._in = os.path.realpath(args._in)
