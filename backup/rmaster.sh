4c7a828822c609912be5ea3a567644fd0e8cfd89
Yuankai He <yh464@cam.ac.uk>
2025-03-10 22:46:15 +0000

    Removed superseded files

--- backup/rmaster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/yh464/.bashrc
-conda deactivate
-conda activate $1
-export OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 VECLIB_MAXIMUM_THREADS=1
-Rscript ${@:2}
