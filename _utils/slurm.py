7a0aabb8e875df350b31751fb44916efbae572b7
Yuankai He <yh464@cam.ac.uk>
2025-09-25 17:40:12 +0100

    Major change to SLURM management, now adds commands to a buffer and writes files only on submission

--- _utils/slurm.py
+++ _utils/slurm.py
@@ -1,7 +1,8 @@
 '''
 Author: Yuankai He
 Correspondence: yh464@cam.ac.uk
-2024-08-22
+Version 1: 2024-08-22
+Version 2: 2025-09-25
 
 This script is a general scaffold to submit jobs in batches
 NB each command is executed separately in the bash script, so
@@ -14,6 +15,7 @@ import argparse
 import re
 import warnings
 import math
+from hashlib import sha256
 
 class array_submitter():
     '''
@@ -47,8 +49,7 @@ class array_submitter():
                  log = '/rds/project/rb643/rds-rb643-ukbiobank2/Data_Users/yh464/logs',
                  tmpdir = '/rds/project/rb643/rds-rb643-ukbiobank2/Data_Users/yh464/temp',
                  parallel = 1, # number of parallel processes, useful for small jobs that need <1 CPU
-                 mode = warnings.warn(DeprecationWarning('Use timeout instead of mode')), # deprecated, use timeout instead
-                 lim = -1, # number of commands per file, default -1
+                 lim = warnings.warn(DeprecationWarning('Command limit will be automatically determined')), # deprecated
                  arraysize = 200, # array size limit, default 2000 for CSD3 cluster, QOS max jobs 500
                  email = True,
                  wallclock = -1, # total time limit per file, default 240 minutes
@@ -67,9 +68,7 @@ class array_submitter():
             parallel *= int(cpu_avail/n_cpu)
             n_cpu *= int(cpu_avail/n_cpu)
 
-        import warnings
         if len(name) > 30:
-            from hashlib import sha256
             name = sha256(name.encode()).hexdigest()[:6] # truncate to 6S characters
             warnings.warn(f'Job name too long, using random name {name}')
         self.name = '_' + name.replace('/','_') + '_0'
@@ -83,7 +82,7 @@ class array_submitter():
         self.n_node = n_node
         self.n_task = n_task
         self.n_cpu = n_cpu
-        if n_cpu < 20: arraysize = min(arraysize, int(500/n_cpu)) # QOS max CPU per user limit is 100
+        if n_cpu < 20: arraysize = min(arraysize, int(500/n_cpu)) # QOS max CPU per user limit is 500
         self.arraysize = arraysize # the default array job size limit is 2000 for SLURM
         self.email = '--mail-type=ALL' if email else ''
         self.account = account
@@ -99,30 +98,20 @@ class array_submitter():
         if type(modules) == type('a'): modules = [modules] # single string
         self.modules = modules
         
-        # internal variables
-        self._blank = True
-        self._count = 1 # number of commands per file
-        self._fileid = 0 # current file id
-        self._nfiles = 0
-        self._jobid = 0
-        
-        # status features
-        self._submitted = False
-        self._slurmid = []
-        
-        # limit of commands per file
-        if lim > 0: self.lim = lim
-        self._mode = 'long' if timeout > 15 else 'short' # long jobs are for > 15 min commands
-        self.wallclock = 240 if timeout > 15 else 60 # timeout = 12 hrs max
-        if wallclock > 0: self.wallclock = min(wallclock,720)
+        # limit of commands per file and wallclock limit
+        self.wallclock = max(timeout, 240) if timeout > 15 else 60
+        if wallclock > 0: self.wallclock = wallclock
+        if account != None and account.find('sl2') >= 0: self.wallclock = min(self.wallclock, 2160)
+        else: self.wallclock = min(self.wallclock, 720) 
+
         # read command line args before specifying limit of commands per file
         import __main__
         if 'args' in dir(__main__): self.config(**vars(__main__.args))
-        # number of commands per file
+
+        # number of *parallel batches* of commands per file
         self.lim = int(self.wallclock/timeout)
         self.lim = max(self.lim, 1) # at least one command per file
-        self.lim *= self.parallel # all parallel processes have the same time limit
-        print(f'Max {self.lim} commands per file, {self.arraysize} files per array job')
+        print(f'Max {self.lim} batches * {self.parallel} commands per file, {self.arraysize} files per array job')
 
         # directories
         self.logdir = f'{log}/{self.name}' # to prevent confusion with other array submissions
@@ -131,6 +120,18 @@ class array_submitter():
         self.tmpdir = f'{tmpdir}/{self.name}' # to prevent confusion with other array submissions
         if not os.path.isdir(self.tmpdir): os.mkdir(self.tmpdir)
         os.system(f'rm -rf {self.tmpdir}/*') # clear temp files from the previous run
+
+        # commands are staged up to an array size limit before a new job array is initialised
+        self.array_cmd_limit = self.arraysize * self.lim * self.parallel
+        self._staged_cmd = []
+        self._blank = True
+        self._count = 0
+        self._nfiles = 0
+        self._jobid = 0
+        
+        # SLURM status properties
+        self._submitted = False
+        self._slurmid = []
     
     # change settings
     def config(self, **kwargs):
@@ -148,7 +149,7 @@ class array_submitter():
             if value == None: continue # skip None values
             if key in numeric_keys: value = int(value) # convert to int
             setattr(self, key, value)
-        if 'jobname' in kwargs.keys() and kwargs['jobname'] != None and self._blank: 
+        if 'jobname' in kwargs.keys() and kwargs['jobname'] != None and len(self._staged_cmd) == 0: 
             setattr(self, 'name', kwargs['jobname'])
         if 'wallclock' in kwargs.keys() and kwargs['wallclock'] != None:
             self.wallclock = min(self.wallclock,720)
@@ -156,26 +157,27 @@ class array_submitter():
         self.lim = max(self.lim, 1) # at least one command per file
         self.lim *= self.parallel # all parallel processes have the same time limit
 
-    # a new file requires a shebang line, so this func resets the file
-    def _newfile(self):
-        fname = f'{self.tmpdir}/{self.name}_{self._fileid}.sh'
-        _file = open(fname,'w')
-        print('#!/bin/bash', file = _file) # shebang line
-        if type(self.env) != type(None): # mamba environment
-            print('source /home/yh464/.bashrc', file = _file)
-            print(f'mamba activate {self.env}', file = _file)
-        for mod in self.modules: # load modules
-            print(f'module load {mod}', file = _file)
-        print(f'cd {self.wd}', file = _file) # enforce working directory
-        _file.close()
-        self._nfiles += 1
-    
+    # adds a command
+    def add(self,*cmd):
+        '''
+        Adds commands to the submitter utility
+        Can pass any number of commands that need to be run in the specific order
+        '''
+        # can append multiple commands at once if they need to be run successively
+        # they are considered a single command and take care of the time limit!
+        if self.submitted: raise ValueError('Cannot add commands after submission')
+        if len(cmd) == 0: return
+        elif len(cmd) > 1: cmd = '(' + ' && '.join(cmd) + ')'; self._staged_cmd.append(cmd)
+        else: cmd = cmd[0]; self._staged_cmd.append(cmd)
+        self._blank = False
+
+    # initialises a new job array
     def _newjob(self):
         # reset cmd count and file id
-        self._count = 1
-        self._fileid = 0
-        self._jobid += 1
+        self._count = 0
         self._nfiles = 0
+        self._jobid += 1
+        self._wrap_name = ''
         
         # reset job name and directories to avoid confusion
         self.name = re.sub(f'_{self._jobid-1}$',f'_{self._jobid}', self.name)
@@ -185,67 +187,22 @@ class array_submitter():
         self.tmpdir = os.path.realpath(f'{self.tmpdir}/..')+f'/{self.name}'
         if not os.path.isdir(self.tmpdir): os.mkdir(self.tmpdir)
         os.system(f'rm -rf {self.tmpdir}/*') # clear temp files from the previous run
-        
-    # adds a command
-    def add(self,*cmd):
-        '''
-        Adds commands to the submitter utility
-        Can pass any number of commands that need to be run in order
-        '''
-        # can append multiple commands at once if they need to be run successively
-        # they are considered a single command and take care of the time limit!
-        if len(cmd) == 0: return # nothing to add -> do not reset the 'blank' flag
-        self._blank = False
-        # preprocess command
-        cmd = ' && '.join(cmd)
-        if self.parallel > 1:
-            cmd += ' &'
-            if self._count % self.parallel == 0: cmd += ' wait'
 
-        if self._mode == 'long':
-            # first iteration: reset files
-            if self._count == 1:
-                self._newfile()
-            
-            # append command
-            fname = f'{self.tmpdir}/{self.name}_{self._fileid}.sh'
-            _file = open(fname,'a')
-            print(cmd, file = _file)
-            _file.close() # I do not want 2000 file handles!
-            
-            # proceed to next file
-            self._fileid += 1
-            
-            # if array size limit is reached, print a new command to the first file
-            if self._fileid >= self.arraysize:
-                if self._count >= self.lim:
-                    self.submit() # because this job is 'full'
-                    self._newjob()
-                else:
-                    self._count += 1
-                    self._fileid = 0
-        
-        elif self._mode == 'short':
-            # for 'short' jobs, submit in 15-min batches
-            if self._count == 1:
-                self._newfile()
-            
-            fname = f'{self.tmpdir}/{self.name}_{self._fileid}.sh'
-            _file = open(fname,'a')
-            print(cmd, file = _file)
-            _file.close() # I do not want 2000 file handles!
-            
-            # if the total time reaches the file size limit, start a new file
-            if self._count >= self.lim:
-                if self._fileid + 1 >= self.arraysize:
-                    self.submit()
-                    self._newjob()
-                else:
-                    self._count = 1
-                    self._fileid += 1
-            else:
-                self._count += 1
+    # dumps staged commands to a new file
+    def _write_file(self, fileid, cmds = []):
+        fname = f'{self.tmpdir}/{self.name}_{fileid}.sh'
+        _file = open(fname,'w')
+        print('#!/bin/bash', file = _file) # shebang line
+        if type(self.env) != type(None): # mamba environment
+            print('source ~/.bashrc', file = _file)
+            print(f'mamba activate {self.env}', file = _file)
+        for mod in self.modules: # load modules
+            print(f'module load {mod}', file = _file)
+        print(f'cd {self.wd}', file = _file) # enforce working directory
+        for cmd in cmds: print(cmd, file = _file) # print commands
+        _file.close()
     
+    # generates the dependency string
     def _write_dep_str(self):
         dep_str = []
         for dep in self.dep: 
@@ -258,10 +215,13 @@ class array_submitter():
                     print(f'Warning: {dep.name} is listed as a dependency and automatically submitted')
                 for idx in dep._slurmid:
                     dep_str.append(f'afterok:{idx}')
+                if len(dep._slurmid) == 0:
+                    dep_str.append('<please run again removing the --debug flag>')
         if len(dep_str) > 0: 
-            return '--dependency '+' '.join(dep_str)
+            return '-d '+','.join(dep_str)
         else: return ''
 
+    # writes the wrapper to be submitted using sbatch
     def _write_wrap(self):
         # master wrapper
         self._wrap_name = f'{self.tmpdir}/{self.name}_wrap.sh'       
@@ -284,7 +244,34 @@ class array_submitter():
         if self.account: print(f'#SBATCH -A {self.account}', file = wrap)
         print(f'bash {self.tmpdir}/{self.name}_'+'${SLURM_ARRAY_TASK_ID}.sh', file = wrap)
         wrap.close()
+
+    # dumps staged commands to a job array
+    def _dump(self):
+        if len(self._staged_cmd) == 0: return
+        cmds_to_dump = self._staged_cmd[:min(len(self._staged_cmd), self.array_cmd_limit)]
+
+        # organise cmds into parallel batches
+        if self.parallel > 1:
+            batches = []
+            for i in range(0, len(cmds_to_dump), self.parallel):
+                batch = cmds_to_dump[i:min(i+self.parallel, len(cmds_to_dump))]
+                batch = ' & '.join(batch + ['wait'])
+                batches.append(batch)
+            cmds_to_dump = batches; del batches
+
+        self._nfiles = min(len(cmds_to_dump), self.arraysize)
+        cmds_to_dump = [cmds_to_dump[i::self._nfiles] for i in range(self._nfiles)]
+        for fileid, cmds in enumerate(cmds_to_dump):
+            self._write_file(fileid, cmds)
+        self._count = len(cmds_to_dump[0]) # number of parallel batches per file
+        self._write_wrap()
+
+        if len(self._staged_cmd) > self.array_cmd_limit:
+            self._submit_single()
+            self._staged_cmd = self._staged_cmd[self.array_cmd_limit:]
+            self._newjob(); self._dump()
     
+    # for debugging
     def _print(self):
         '''
         Prints one sample command file and the sbatch command
@@ -296,10 +283,9 @@ class array_submitter():
         import os
         os.system(f'cat {self.tmpdir}/{self.name}_0.sh')
         print(f'\n\nbash {self.tmpdir}/{self.name}_0.sh\n\n')
-        self._write_wrap()
         
         # debug command also outputs the submit command
-        time = self.timeout * math.ceil(self._count / self.parallel)
+        time = self.timeout * self._count
         email = '--mail-type=ALL' if self.email else ''
         account = f'-A {self.account}' if self.account else ''
         print(f'sbatch -N {self.n_node} -n {self.n_task} -c {self.n_cpu} '+
@@ -307,6 +293,7 @@ class array_submitter():
                   f'-o {self.logdir}/{self.name}_%a.log -e {self.logdir}/{self.name}_%a.err'+ # %a = array index
                   f' --array=0-{self._nfiles-1} {self._write_dep_str()} {self._wrap_name}') 
     
+    # splash screen
     def _splash(self, jobid):
         msg = []
         msg.append('#' * 100)
@@ -318,26 +305,15 @@ class array_submitter():
         msg.append(f'    CPUs:       {self.n_cpu}')
         msg.append(f'    # files:    {self._nfiles}')
         msg.append(f'    Parallel:   {self.parallel}')
-        msg.append(f'    Dependency: {self._write_dep_str().replace('--dependency ','') if self._write_dep_str() != "" else "None"}')
+        msg.append(f'    Dependency: {re.sub("^-d ","", self._write_dep_str()) if self._write_dep_str() != "" else "None"}')
         msg.append(f'    Job ID:     {jobid}')
         msg.append('#' * 100)
         if not self._blank: print('\n'.join(msg))
 
-    def submit(self):
-        '''
-        Submits all commands to the cluster (SLURM manager)
-        '''
-        # if debug mode is on, debug instead
-        if self._blank: return
-        if self.debug: self._print(); return
-        if self.intr: import os; os.system(f'for x in {self.tmpdir}/*.sh; do bash $x; done'); return
-        
-        if self.parallel > 1:
-            for i in range(self._nfiles):
-                _file = open(f'{self.tmpdir}/{self.name}_{i}.sh','a')
-                print('wait', file = _file)
-                _file.close()
-        self._write_wrap()
+    # submits a single job array
+    def _submit_single(self):
+        if self.debug: self._print(); return # debug mode -> print only
+        if self.intr: os.system(f'for x in {self.tmpdir}/*.sh; do bash $x; done'); return
         time = self.timeout * math.ceil(self._count / self.parallel)
         time = min(time, 720)
         email = '--mail-type=ALL' if self.email else ''
@@ -353,7 +329,16 @@ class array_submitter():
         self._splash(jobid)
         self._slurmid.append(jobid)
         self.submitted = True
-        return jobid
+
+    # master wrapper for job submission
+    def submit(self):
+        '''
+        Submits all commands to the cluster (SLURM manager)
+        '''
+        if self._blank: return
+        self._dump()
+        self._submit_single()
+        self._staged_cmd = []
 
 class slurm_parser(argparse.ArgumentParser):
     '''
