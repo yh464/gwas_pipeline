bb92034c74555d291cfabfb3e58293a4dc6a547f
Yuankai He <yh464@cam.ac.uk>
2025-11-25 17:34:17 +0000

    bug fix

--- _plots/__init__.py
+++ _plots/__init__.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.realpath("..")) # for cross-import between _utils, _plugins, _plots
+
 from .corr_heatmap import *
 from .colourcode_scatterplot import *
 from .regplot import *
---
+++ _utils/__init__.py
@@ -0,0 +1,2 @@
+import sys, os
+sys.path.append(os.path.realpath("..")) # for cross-import between _utils, _plugins, _plots
