c575722880b313d3a61cb4f070178da2a9b5bbf8
Yuankai He <yh464@cam.ac.uk>
2025-11-25 17:37:23 +0000

    Revert "bug fix"
    This reverts commit bb92034c74555d291cfabfb3e58293a4dc6a547f.

--- _plots/__init__.py
+++ _plots/__init__.py
@@ -1,6 +1,3 @@
-import sys, os
-sys.path.append(os.path.realpath("..")) # for cross-import between _utils, _plugins, _plots
-
 from .corr_heatmap import *
 from .colourcode_scatterplot import *
 from .regplot import *
--- _plugins/__init__.py
+++
@@ -1,2 +0,0 @@
-import sys, os
-sys.path.append(os.path.realpath("..")) # for cross-import between _utils, _plugins, _plots
