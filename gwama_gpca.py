e21fde1dd823704552cad909734035fe362aea41
Yuankai He <yh464@cam.ac.uk>
2026-02-08 05:53:23 +0000

    test

--- gwama_gpca.py
+++ gwama_gpca.py
@@ -20,21 +20,22 @@ from tqdm import tqdm
 from _utils.path import find_gwas
 from _utils.plugins.logparser import crosscorr_parse
 from _utils import logger
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
 log = logger.logger()
 
 def read_sumstats(input_args):
     g, p, in_dir, weight = input_args
     df = pd.read_table(f'{in_dir}/{g}/{p}.fastGWA', usecols = 
         lambda x: x.upper() in (['CHR','SNP','POS','A1','A2','BETA','OR','SE','N','AF1']),
-        index_col = ['SNP']).sort_values(by = ['CHR','POS','A1','A2'])
+        index_col = ['SNP']).copy()
+    df = df.loc[~df.index.duplicated(keep = False), :].sort_index()
     df.columns = [x.upper() for x in df.columns]
     if 'OR' in df.columns and not 'BETA' in df.columns: df['BETA'] = np.log(df['OR'])
     
-    n = df['N'].rename((g, p))
-    w = (weight * (n ** 0.5)).rename((g, p))
-    z = (df['BETA'] * w / df['SE']).rename((g, p))
-    af = (df['AF1'] * n).rename((g, p))
+    n = df['N']
+    w = (weight * (n ** 0.5))
+    z = (df['BETA'] * w / df['SE'])
+    af = (df['AF1'] * n)
     snpinfo = df[['CHR','POS','A1','A2']]
     return w, z, af, n, snpinfo
 
@@ -57,37 +58,41 @@ def main(args):
 
     # read summary stats and aggregate weighted Z-scores
     parallel_args = [(g, p, args._in, weights.loc[(g,p)]) for g, p in pheno]
-    with Pool(16) as pool:
+    with Pool(min(16, len(parallel_args))) as pool:
         out = list(tqdm(pool.imap(read_sumstats, parallel_args), total = len(parallel_args), desc = 'Reading summary statistics and aggregating weighted Z-scores'))
     weight_list, z_list, af_list, n_list, snpinfo_list = zip(*out)
+    del out
 
     log.log('Merging variant information across all summary statistics')
-    snpinfo = pd.concat(snpinfo_list, axis = 0).drop_duplicates().sort_values(by = ['CHR','POS','A1','A2'])
+    snpinfo = pd.concat(snpinfo_list, axis = 0).drop_duplicates().sort_index()
     if snpinfo.duplicated(['CHR','POS']).any():
         log.warn(f'{snpinfo.duplicated(["CHR","POS"]).sum()} variants have different alleles across files')
         snpinfo.loc[snpinfo.duplicated(['CHR','POS'], keep = False), :].to_csv(args.out.replace('.fastGWA', '.missnp'), sep = '\t', index = False)
         log.log(f'List of variants with inconsistent alleles across files written to {args.out.replace(".fastGWA", ".missnp")}')
         snpinfo = snpinfo.drop_duplicates(['CHR','POS'], keep = False)
         log.log(f'{snpinfo.shape[0]} variants with consistent alleles across files retained for meta-analysis')
+    del snpinfo_list
 
     log.log('Calculating meta-analytic Z-scores')
-    n_total = pd.concat(list(n_list), axis = 1).fillna(0).sum(axis = 1)
-    out_z = pd.concat(list(z_list), axis = 1).fillna(0).sum(axis = 1)
-    del n_list, z_list
+    n_total = pd.concat(list(n_list), axis = 1, ignore_index = True).fillna(0).sum(axis = 1).rename('N')
+    del n_list
+    out_z = pd.concat(list(z_list), axis = 1, ignore_index = True).fillna(0).sum(axis = 1).rename('Z')
+    del z_list
 
     # adjust AF1
     log.log('Estimating allele frequencies weighted by sample size')
-    af_list = pd.concat(list(af_list), axis = 1).fillna(0)
+    af_list = pd.concat(list(af_list), axis = 1, ignore_index = True).fillna(0)
     af1 = af_list.sum(axis = 1).rename('AF1') / n_total
     del af_list
 
     # for each SNP, divide the weighted Z-score by sqrt(weight[:,SNP].T dot gcov_int dot weight[:,SNP])
-    weight = pd.concat(list(weight_list), axis = 1).fillna(0)
-    del out, weight_list
+    weight = pd.concat(list(weight_list), axis = 1, ignore_index = True).fillna(0)
+    weight.columns = pd.MultiIndex.from_tuples(pheno, names = ['group','pheno'])
+    del weight_list
     gcovint = gcovint.loc[weight.columns, weight.columns].fillna(0).values
     div_coef = ((weight.values @ gcovint) * weight.values).sum(axis = 1) ** 0.5
     out_z = out_z / div_coef
-    out = pd.concat([snpinfo, af1, out_z, n_total], axis = 1)
+    out = pd.concat([snpinfo, af1, out_z, n_total], axis = 1, join = 'inner').sort_values(['CHR','POS'])
     out['P'] = sts.norm.sf(abs(out_z)) * 2
     out['BETA'] = out_z / n_total / (af1 * (1-af1)) ** 0.5
     out['SE'] = out['BETA'] / out_z
