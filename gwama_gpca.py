f8b3a55bc39e47a2d383d054bd9a5552c786f660
Yuankai He <yh464@cam.ac.uk>
2026-02-14 14:13:02 +0000

    performance twist

--- gwama_gpca.py
+++ gwama_gpca.py
@@ -39,7 +39,6 @@ def sep_chr(input_args):
     })
     for chrom, df_chr in df.groupby('CHR'):
         os.makedirs(f'{tmpdir}/{chrom}/{g}', exist_ok = True)
-        df_chr.to_csv(f'{tmpdir}/{chrom}/{g}/{p}.fastGWA', sep = '\t', index = True, header = True)
         df_chr.to_parquet(f'{tmpdir}/{chrom}/{g}/{p}.parquet', index = True)
     gc.collect()
     return df['CHR'].unique().tolist()
