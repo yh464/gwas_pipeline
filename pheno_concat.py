4004cc358c9e461ebb58624eda1e2b1b3a1c119a
Yuankai He <yh464@cam.ac.uk>
2026-02-22 00:10:30 +0000

    bug fix

--- pheno_concat.py
+++ pheno_concat.py
@@ -23,6 +23,7 @@ Changelog:
 '''
 
 import pandas as pd
+import numpy as np
 import os
 from _utils.logger import logger
 log = logger()
@@ -51,7 +52,10 @@ def main(args):
             tmp = tmp.pivot_table(columns = 'pheno', index = 'EID', values = 'value')
             tmp.to_csv(f'{pg}.unstd.txt', sep = '\t', index = False)
             for c in tmp.columns:
-                if tmp[c].std() <= 0: log.warn(f'Phenotype {c} has zero variance in group {pg} and will not be scaled'); continue
+                if (tmp[c] == np.inf).any():
+                    log.warn(f'Phenotype {pg}/{c} has infinite values, setting to NaN')
+                    tmp[c] = tmp[c].replace(np.inf, np.nan)
+                if tmp[c].std() <= 0: log.warn(f'Phenotype {pg}/{c} has zero variance and will not be scaled'); continue
                 tmp[c] /= tmp[c].std()
             tmp.insert(0, column = 'FID', value = tmp.index)
             tmp.insert(1, column = 'IID', value = tmp.index)
