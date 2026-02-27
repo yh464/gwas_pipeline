745a21d174acf27c511ce82f3b4441b11e02211f
Yuankai He <yh464@cam.ac.uk>
2026-02-27 04:45:39 +0000

    bug fix

--- bfile_merge_autosomes.sh
+++ bfile_merge_autosomes.sh
@@ -3,4 +3,4 @@
 # inspired by https://apol1.blogspot.com/2013/10/merging-plink-binary-files.html
 cp chr1.fam autosomes.fam
 for chrom in {1..22}; do cat chr${chrom}.bim; done > autosomes.bim
-(echo -en "\x6C\x1B\x01"; for chrom in {1..22}; do tail -c chr${chrom}.bed; done) > autosomes.bed
+(echo -en "\x6C\x1B\x01"; for chrom in {1..22}; do tail -c +4 chr${chrom}.bed; done) > autosomes.bed
