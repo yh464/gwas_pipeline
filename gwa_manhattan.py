3c7a57a764cea8b654338e39302086d526ac2a0f
Yuankai He <yh464@cam.ac.uk>
2026-03-02 15:39:31 +0000

    bug fix

--- gwa_manhattan.py
+++ gwa_manhattan.py
@@ -13,7 +13,7 @@ parser.add_argument('-i','--in', dest = '_in', help = 'GWA file directory',
 parser.add_argument('-o','--out', dest = 'out', help = 'output directory',
   default = '../manhattan/')
 parser.add_argument('-p','--pval', help = 'p-value threshold', type = float, default = 5e-8)
-parser.add_argument('-a','--autosome_only',dest = 'a', help = 'exclude sex chromosomes',
+parser.add_argument('-a','--autosome_only', help = 'exclude sex chromosomes',
   default = False, action = 'store_true')
 parser.add_argument('-f','--force',dest = 'force', help = 'force output',
   default = False, action = 'store_true')
