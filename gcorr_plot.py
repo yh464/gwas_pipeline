eefad8aec07fa71ab49c3b12eb843a21517e68af
Yuankai He <yh464@cam.ac.uk>
2026-03-02 00:50:08 +0000

    bug fix

--- gcorr_plot.py
+++ gcorr_plot.py
@@ -44,9 +44,9 @@ def main(args):
     # tabular output, wide and long
     norm = normaliser()
     if len(args.p2) > 0:
-        fout = f'{args.out}/crosscorr_' + ('_'.join(args.p1)+'.'+'_'.join(args.p2)).replace('/','_')
+        fout = f'{args.out}/crosscorr_' + ('_'.join([g for g,_ in gwa1])+'.'+'_'.join([g for g,_ in gwa2])).replace('/','_')
     else: 
-        fout = f'{args.out}/corr_' + ('_'.join(args.p1)).replace('/','_')
+        fout = f'{args.out}/corr_' + ('_'.join([g for g,_ in gwa1])).replace('/','_')
         summary_rev = summary.copy()
         summary_rev[['group1','pheno1','group2','pheno2']] = summary[['group2','pheno2','group1','pheno1']]
         summary = pd.concat([summary, summary_rev]).sort_values(['group1','pheno1','group2','pheno2']).drop_duplicates()
