1d1ac783e4003f3e6d369cc0e61bcdb096227c3b
Yuankai He <yh464@cam.ac.uk>
2026-03-11 16:50:51 +0000

    test

--- gwama_gpca.py
+++ gwama_gpca.py
@@ -109,8 +109,10 @@ def gwama(pheno, weight_list, z_list, af_list, n_list, snpinfo_list, gcovint):
     weight = pd.concat(list(weight_list), axis = 1, ignore_index = True).fillna(0)
     out_z = (pd.concat(list(z_list), axis = 1, ignore_index = True).fillna(0) * weight.values).sum(axis = 1).rename('Z')
     weight.columns = pd.MultiIndex.from_tuples(pheno, names = ['group','pheno'])
+    print(weight)
     gcovint = gcovint.loc[weight.columns, weight.columns].fillna(0).values
     div_coef = np.einsum('ij, jk, ik -> i', weight.values, gcovint, weight.values, optimize = 'optimal') ** 0.5
+    print(div_coef)
     out_z = out_z / div_coef
     out = pd.concat([snpinfo, af1, out_z, n_total], axis = 1, join = 'inner').sort_values(['CHR','POS'])
     out['P'] = sts.norm.sf(abs(out['Z'])) * 2
