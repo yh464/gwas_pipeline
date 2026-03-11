6671aa9f4e65a113768b3ef4e3d7ed61d974c3dd
Yuankai He <yh464@cam.ac.uk>
2026-03-11 16:03:13 +0000

    bug fix

--- gwama_gpca.py
+++ gwama_gpca.py
@@ -123,7 +123,7 @@ def main(args):
         log.log(f'    {g} {p} (h2 = {h2.loc[(g,p)]:.4f})')
     log.log(f'{len(pheno)} phenotypes with positive heritability estimates retained for analysis')
     rg = rg.loc[pheno, pheno]
-    gcovint = corr.pivot(index = ['group1','pheno1'], columns = ['group2','pheno2'], values = 'gcov_int').loc[pheno, pheno]
+    gcovint = corr.pivot(index = ['group1','pheno1'], columns = ['group2','pheno2'], values = 'gcov_int').loc[pheno, pheno].fillna(gcovint.values.T)
     if args.pca: # use PCA to estimate weights
         pc1 = np.real(np.linalg.eig(rg.fillna(0).values)[1][:,0])
         weights = pd.Series(pc1, index = rg.index)
