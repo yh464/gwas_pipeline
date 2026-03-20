b50e75df37c2458719a75e6d8533ffb26dbf6986
Yuankai He <yh464@cam.ac.uk>
2026-03-20 15:06:51 +0000

    bug fix

--- _utils/genetools.py
+++ _utils/genetools.py
@@ -45,7 +45,7 @@ def fetch_rest(ensg, build = 'hg19'):
     server = "https://rest.ensembl.org" if build in ['hg38','grch38'] else "https://grch37.rest.ensembl.org"
     ext = "/lookup/id"
     headers = {"Content-Type": "application/json", 'Accept': "application/json"}
-    r = requests.post(server+ext, headers = headers, data = '{"ids": ' + str(ensg) + '}')
+    r = requests.post(server+ext, headers = headers, data = '{"ids": ["' + '","'.join(ensg) + '"]}')
     if not r.ok:
         r.raise_for_status()
         log.error(f'Failed to fetch gene information from ENSEMBL REST API: {r.text}')
