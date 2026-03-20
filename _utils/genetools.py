6dfc2ab916c586e49f6f15db7d982c8f739d2a5b
Yuankai He <yh464@cam.ac.uk>
2026-03-20 16:09:52 +0000

    test

--- _utils/genetools.py
+++ _utils/genetools.py
@@ -13,6 +13,7 @@ import pandas as pd
 import numpy as np
 import scipy.stats as sts
 import warnings
+from multiprocessing import Pool, cpu_count
 from .logger import logger
 log = logger()
 
@@ -41,15 +42,15 @@ def _regenerate_ref(file, build = 'hg19'):
 def fetch_rest(ensg, build = 'hg19', filter = True):
     '''Fetches the REST API of ENSEMBL to get gene information'''
     ensg = [e.split('.')[0] for e in ensg]
+    ensg = list(set(ensg)) # ensure uniqueness
     log.log(f'Fetching gene information for {len(ensg)} genes from ENSEMBL REST API')
     server = "https://rest.ensembl.org" if build in ['hg38','grch38'] else "https://grch37.rest.ensembl.org"
     ext = "/lookup/id"
     headers = {"Content-Type": "application/json", 'Accept': "application/json"}
     # ENSEMBL REST API only allows up to 1000 genes per request, so we need to split the list into chunks of 1000
-    out = []
-    for i in range(0, len(ensg), 1000):
-        chunk = ensg[i:min(i+1000, len(ensg))]
-        r = requests.post(server+ext, headers = headers, json = {"ids": chunk})
+    if len(ensg) <= 1000:
+        out = []
+        r = requests.post(server+ext, headers = headers, json = {"ids": ensg})
         if not r.ok:
             r.raise_for_status()
             log.error(f'Failed to fetch gene information from ENSEMBL REST API: {r.text}')
@@ -62,7 +63,17 @@ def fetch_rest(ensg, build = 'hg19', filter = True):
                 DIR = '+' if info['strand'] == 1 else '-',
                 LABEL = info['display_name']),
                 index = [gene]))
-            except: log.warn(f'Failed to fetch information for gene {gene}'); continue
+            except: continue
+    else:
+        batches = [(ensg[i:min(i+1000, len(ensg))], build, filter) for i in range(0, len(ensg), 1000)]
+        with Pool(min(cpu_count(), 16)) as pool:
+            out = pool.starmap(fetch_rest, batches)
+    if len(out) == 0:
+        out = pd.DataFrame(columns = ['CHR','START','STOP','DIR','LABEL'], dtype =
+            {'CHR': 'int' if filter else 'category', 'START': 'int', 'STOP': 'int', 'DIR': 'category'}
+        )
+        out.index.name = 'GENE'
+        return out
     out = pd.concat(out, axis = 0)
     out.CHR = out.CHR.replace('X', '23').replace('Y', '24').replace('MT', '26').replace('XY', '25')
     if filter: out = out.loc[out.CHR.isin([str(c) for c in range(1,27)]),:].astype(
