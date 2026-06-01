fe441e2cb8d0b57d622f69f37d11c0d0b67c1a7d
Yuankai He <yh464@cam.ac.uk>
2026-06-01 23:50:52 +0100

    update gsmap plot aesthetics

--- sc_gsmap_plot.py
+++ sc_gsmap_plot.py
@@ -39,13 +39,13 @@ def main(args):
 
             if not os.path.isfile(h5ad): log.warn(f'Missing h5ad file for {s}, skipping'); continue
             if not os.path.isfile(gsmap_output):
-                log.warn(f'Missing spatial LDSC output file for {s}')
+                log.warn(f'Missing spatial LDSC output file for {g}/{p} in {s}')
                 continue
             
             for ct in args.cell_type + ['cell_type']:
                 gsmap_cauchy_ct = f'{args._in}/{g}/{p}/{s}_spatial_ldsc.{ct}.cauchy.csv.gz'
                 if not os.path.isfile(gsmap_cauchy_ct): 
-                    log.warn(f'Missing Cauchy combination output file for {ct} in {s}')
+                    log.warn(f'Missing Cauchy combination output file for {g}/{p} in {s}/{ct}')
                     continue
                 all_cauchy[ct].append(pd.read_csv(gsmap_cauchy_ct, index_col = 0, usecols = ['annotation','p_cauchy']).rename(columns = {'p_cauchy':s}))
 
@@ -58,7 +58,7 @@ def main(args):
             if df.shape[0] == 0: log.warn(f'No overlapping spots between {h5ad} and {gsmap_output}, skipping'); continue
             df['logp'] = -np.log10(df['p'])
             df.loc[df['logp'] < 0, 'logp'] = 0
-            point_size = min(max(10000/df.shape[0], 0.1), 25)
+            point_size = min(max((10000/df.shape[0])**2, 0.1), 64)
             colourcode_scatterplot.scatterplot_noaxis(df['x'], df['y'], df['logp'], palette = redgrey, 
                 s = point_size, rep = False, vname = r"$-log_{10}{(P)}$", vmin = 0)
             plt.savefig(out_fig, dpi = 400)
