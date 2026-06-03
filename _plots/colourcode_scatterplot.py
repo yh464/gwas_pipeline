cdfd38696b45d44df5f196582d2b76961b4f4984
Yuankai He <yh464@cam.ac.uk>
2026-06-03 04:37:41 +0100

    test

--- _plots/colourcode_scatterplot.py
+++ _plots/colourcode_scatterplot.py
@@ -63,15 +63,16 @@ def scatterplot_noaxis(x, y, v, *, full = True, palette = None, s = 'auto', rep 
   fig = plt.figure(figsize = (5.3,5))
   ax = fig.add_axes((0.3/5.3, 0.3/5, 4.4/5.3, 4.4/5))
   if s == 'auto':
-    # estimate distance between points in the x-y plane
-    from scipy.spatial.distance import pdist
-    if df.shape[0] > 1 and df.shape[0] < 50000:
-      dists = pdist(df[['x','y']].values)
-      min_dist = np.nanquantile(dists, 0.05)
-      axis_range = max(df['x'].max() - df['x'].min(), df['y'].max() - df['y'].min())
-      s = (min_dist/axis_range)**2 * 144
-    elif df.shape[0] >= 50000: s = 0.1
-    else: s = 10
+    # # estimate distance between points in the x-y plane
+    # from scipy.spatial.distance import pdist
+    # if df.shape[0] > 1 and df.shape[0] < 50000:
+    #   dists = pdist(df[['x','y']].values)
+    #   min_dist = np.nanquantile(dists, 0.05)
+    #   axis_range = max(df['x'].max() - df['x'].min(), df['y'].max() - df['y'].min())
+    #   s = (min_dist/axis_range)**2 * 144
+    # elif df.shape[0] >= 50000: s = 0.1
+    # else: s = 10
+    s = 50000 / df.shape[0]
     s = min(max(s, 0.1), 36) # set a reasonable range for point size
   sns.scatterplot(data = df, x = 'x', y = 'y', hue = 'v', palette = use_palette, s = s, ax = ax, edgecolor = None, linewidth = 0, 
       legend = legend, rasterized = True, **kwargs)
