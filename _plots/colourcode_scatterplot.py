44f02e795124143ac571e87a4fc7bb9d8be37b35
Yuankai He <yh464@cam.ac.uk>
2026-06-03 03:48:40 +0100

    test

--- _plots/colourcode_scatterplot.py
+++ _plots/colourcode_scatterplot.py
@@ -65,14 +65,14 @@ def scatterplot_noaxis(x, y, v, *, full = True, palette = None, s = 'auto', rep 
   if s == 'auto':
     # estimate minimum distance between points in the x-y plane
     from scipy.spatial.distance import pdist
-    if df.shape[0] > 1:
+    if df.shape[0] > 1 and df.shape[0] < 50000:
       dists = pdist(df[['x','y']].values)
       min_dist = np.nanquantile(dists, 0.01) # use 1st percentile of distances to avoid outliers dominating
       axis_range = max(df['x'].max() - df['x'].min(), df['y'].max() - df['y'].min())
-      s = (min_dist*2.5/axis_range)**2 / np.pi
-    else:
-      s = 10
-    s = min(max(s, 0.01), 64) # set a reasonable range for point size
+      s = (min_dist*180/axis_range)**2 # scale point size based on minimum distance and axis range, assuming 72 points per inch
+    elif df.shape[0] >= 50000: s = 0.1
+    else: s = 10
+    s = min(max(s, 0.1), 64) # set a reasonable range for point size
   sns.scatterplot(data = df, x = 'x', y = 'y', hue = 'v', palette = use_palette, s = s, ax = ax, edgecolor = None, linewidth = 0, 
       legend = legend, rasterized = True, **kwargs)
   ax.axis('off')
