1f638b2a47b8fbb1c2344560e6bf78a274e830b9
Yuankai He <yh464@cam.ac.uk>
2026-06-07 19:21:14 +0100

    bug fix

--- sc_scdrs_batch.py
+++ sc_scdrs_batch.py
@@ -76,9 +76,9 @@ def main(args):
                         (' -f' if args.force else '')
                     )
                 if os.path.isfile(f'{out_prefix}.score.txt') and os.path.isfile(f'{out_prefix}.enrichment.txt') \
-                    and os.path.isfile(f'{out_prefix}.score.png') and \
+                    and os.path.isfile(f'{out_prefix}.score.pdf') and \
                     (not args.downstream or (os.path.isfile(f'{out_prefix}.downstream.txt') and \
-                        os.path.isfile(f'{out_prefix}.downstream.revigo.txt') and os.path.isfile(f'{args.out}.pseudotime.png'))) \
+                        os.path.isfile(f'{out_prefix}.downstream.revigo.txt') and os.path.isfile(f'{args.out}.pseudotime.pdf'))) \
                     and not args.force: continue
                 cmd = ['python', 'sc_scdrs.py', '-i', weights_file, '-n', 
                        f'{nsigs[-1]}' if args.nsig < 0 else f'{args.nsig:.0f}',
