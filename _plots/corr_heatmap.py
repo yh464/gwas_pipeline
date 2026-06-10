30c576a60c2230f75f47f82396bc5f828fbe460e
Yuankai He <yh464@cam.ac.uk>
2026-06-10 01:25:37 +0100

    bug fix

--- _plots/corr_heatmap.py
+++ _plots/corr_heatmap.py
@@ -31,6 +31,9 @@ def corr_heatmap(summary, sort = True, absmax = None, autocor = False, annot = '
         5th column: correlation value
         and may contain a 'p' and 'q' column somewhere in the data frame
     '''
+
+    summary = summary.copy()
+
     # style sheet
     sns.set_theme(style = 'whitegrid')
 
@@ -138,7 +141,7 @@ def corr_heatmap(summary, sort = True, absmax = None, autocor = False, annot = '
                 spine.set_visible(False)
             
             # diagonal line
-            if i == j and autocor and i != '': ax[i,j].axline((0,0), slope = 1, c = 'k', zorder = 0)
+            if i == j and g1 == g2 and g1 != '': ax[i,j].axline((0,0), slope = 1, c = 'k', zorder = 0)
     
     if autocor: ax[0,0].annotate(annot,(0,0),xytext=(-4,-3.5), rotation = 45)
     
@@ -166,6 +169,7 @@ def corr_heatmap_wide_format(corr_mat, **kwargs):
     Docstring for corr_heatmap_wide_format
     Converts a wide-format correlation matrix into long format and calls corr_heatmap.
     '''
+    corr_mat = corr_mat.copy()
     index_name = corr_mat.index.name if corr_mat.index.name is not None else ''
     columns_name = corr_mat.columns.name if corr_mat.columns.name is not None else ''
     corr_mat['index_tmp'] = corr_mat.index
